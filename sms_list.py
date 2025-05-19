bc29bb2c8602476915600fe8de7ea485b15fe8de
htglinh <165361405+htglinh@users.noreply.github.com>
2025-05-19 15:25:55 +0700

    Delete sms_list.py

--- sms_list.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-class SMSList(models.Model):
-    _name = 'sms.list'
-    _description = 'SMS Recipient List'
-
-    name = fields.Char(string='List Name', required=True)
-    subscription_ids = fields.One2many('sms.contact.subscription', 'list_id', string='Subscriptions')
