3c4086e4234a8e28419063bb370be4fb2ffa6b04
htglinh <165361405+htglinh@users.noreply.github.com>
2025-05-19 15:26:18 +0700

    Delete sms_contact_subscription.py

--- sms_contact_subscription.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-class SMSContactSubscription(models.Model):
-    _name = 'sms.contact.subscription'
-    _description = 'Contact - List Subscription'
-
-    contact_id = fields.Many2one('sms.contact', required=True)
-    list_id = fields.Many2one('sms.list', required=True)
