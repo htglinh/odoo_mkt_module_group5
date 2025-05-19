6cd82fbb538b0dbbde0ce009d034c1d3c576af99
htglinh <165361405+htglinh@users.noreply.github.com>
2025-05-19 15:25:32 +0700

    Delete __init__.py

--- __init__.py
+++
@@ -1,6 +0,0 @@
-from . import sms_list
-from . import sms_contact
-from . import sms_contact_subscription
-from . import sms_marketing
-from . import sms_marketing_message
-from . import sms_marketing_line
